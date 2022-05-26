5e3f1e5f520bbefa255cd3166145b47c9206e5cb
Javier <javier.alfaro.jara@gmail.com>
2022-05-26 10:33:51 -0600

    commit equis

--- src/components/ChatComponent.jsx
+++ src/components/ChatComponent.jsx
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types'
 import SendMessage from './SendMessage';
 import '../style/chatcomponent.css'
 
+// Dialogo
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
 
 //firebase
 import { database, auth } from '../firebaseConfig';
@@ -15,6 +23,16 @@ import MediaText from './MediaText';
 
 
 const ChatComponent = ({idConversation}) =>{
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const [messages, setMessages] = useState([]);
   //let boolean=false
@@ -46,9 +64,28 @@ const ChatComponent = ({idConversation}) =>{
                     //typeFile === 'image' ? <MediaImg /> : <MediaVideo />
                   }
                   <div className='botonesMensajes'>
-                    <input type="button" value={"Editar"}/>
-                    <input type="button" value={"Eliminar"}/>  
+                    <input type="button" value={"Editar"} onClick={handleClickOpen}/>
+                    <input type="button" value={"Eliminar"}/>
                   </div>
+
+                  <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>Editar mensaje</DialogTitle>
+                    <DialogContent>
+                      <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Mensaje editado"
+                        type="email"
+                        fullWidth
+                        variant="standard"
+                      />
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={handleClose}>Cancelar</Button>
+                      <Button onClick={handleClose}>Editar</Button>
+                    </DialogActions>
+                  </Dialog>
                   
                 </div>
                 
